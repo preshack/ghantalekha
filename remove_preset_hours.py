a63d578e213cfc6ec1502d9f849a8115eeaa6613
Preshak Bhattarai <preshakbhattarai534@gmail.com>
2026-02-15 10:45:21 -0600

    Cleanup: Remove local test and utility scripts

--- remove_preset_hours.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.extensions import db
-from app.models.attendance import Attendance
-
-app = create_app()
-with app.app_context():
-    db.session.query(Attendance).update({'work_duration_minutes': None})
-    db.session.commit()
-    print('All preset hours removed.')
